41234eb992efb4c5dbed4980cad7481391c587dc
deepak_chandola <deepakchandola717@gmail.com>
2019-12-23 11:47:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
+    <Router>
+      <Switch>
     <div className="App">
       <header className="App-header">
-        
+        <Route path={['/', '/home']} exact component={Home}/>
       </header>
     </div>
+    </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import DtTitle from '../data/dt-title.svg';
+import EnterBackground from '../data/enter-background.svg';
+
+const Home = () => {
+
+    const buttonStyle={width:'16rem', padding:'0.5vw', margin:'0.4vw',zIndex:'2', backgroundColor:'white', borderRadius:'0.5rem', color:'#1A5261', cursor:'pointer'};
+
+    return ( 
+        <>
+            <div style={{display:'flex', flexDirection:'column'}}>
+                <div style={{width:'100vw', height:'20vh', backgroundColor:'#1A5261'}}/>
+                <img style={{position:'absolute', zIndex:'1', top:'18vh', left:'2vw', height:'28vh', width:'94vw'}} src={DtTitle} alt="deeType"/>
+                <div style={{width:'100vw', height:'35vh', backgroundColor:'#2A2136'}}/>
+                <div style={{width:'100vw', height:'45vh', backgroundColor:'#823030'}}>
+                <img style={{position:'absolute', zIndex:'0', bottom:'10.0vh', right:'0', height:'40vh', overflow:''}} src={EnterBackground} alt="deeType"/>
+
+                    <div style={{display:'flex', width:'90vw',padding:'3rem 0', margin:'auto', justifyContent:'space-around'}}>
+                        <div style={{display:'flex', flexDirection:'column'}}>
+                            <div style={buttonStyle}>choose file</div>
+                            <div style={buttonStyle}>copy-paste text</div>
+                            <div style={{display:'flex'}}>
+                                <div style={{height:'2rem', width:'4rem', zIndex:'2', backgroundColor:'#2A2136', border:'solid white 3px', padding:'0.2rem', margin:'1rem'}}></div>
+                                <span style={{alignSelf:'center',zIndex:'2'}}>Minutes</span>
+                            </div>
+                        </div>
+                        <span style={{alignSelf:'center', fontSize:'7vw', margin:'1rem', cursor:'pointer',zIndex:'2', border:'dotted white 2px', padding:'1rem', borderRadius:'1rem'}}>ENTER</span>
+                    </div>
+                </div>
+            </div>
+        </>
+     );
+}
+ 
+export default Home;
